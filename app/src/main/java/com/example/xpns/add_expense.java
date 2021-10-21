dc9d88cc2f0a1d7e0526903cbd54f6afdcca3425
kaly1a <kalyaninayak1221@gmail.com>
2021-10-21 21:33:14 +0530

    date picker

--- app/src/main/java/com/example/xpns/add_expense.java
+++ app/src/main/java/com/example/xpns/add_expense.java
@@ -1,12 +1,17 @@
 package com.example.xpns;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.text.InputType;
+import java.util.Calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,13 +25,15 @@ import java.util.Map;
 
 public class add_expense extends AppCompatActivity {
 
-    FirebaseFirestore fStore ;
+    FirebaseFirestore fStore;
     String userID;
-    TextView description,amount;
+    TextView description, amount;
     Button submitButton;
     RadioGroup radioGroup;
     private FirebaseAuth mAuth;
     RadioButton selectedRadioButton;
+    DatePickerDialog picker;
+    EditText eText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,36 +46,54 @@ public class add_expense extends AppCompatActivity {
         description = (TextView) findViewById(R.id.description);
         amount = (TextView) findViewById(R.id.amount);
         submitButton = (Button) findViewById(R.id.submitButton);
-        radioGroup = (RadioGroup) findViewById((R.id.radioGroupSplitExpense));
+        radioGroup = (RadioGroup) findViewById(( R.id.radioGroupSplitExpense ));
 
-    }
+        eText = (EditText) findViewById(R.id.editTextDate);
+        eText.setInputType(InputType.TYPE_NULL);
+        eText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(add_expense.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                eText.setText(dayOfMonth + "/" + ( monthOfYear + 1 ) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
 
 
-    public void addExpenseButton(View view) {
+            public void addExpenseButton(View view) {
 
-        int selectedRadioButtonId = radioGroup.getCheckedRadioButtonId();
-        if (selectedRadioButtonId != -1) {
-            selectedRadioButton = findViewById(selectedRadioButtonId);
-            String selectedRbText = selectedRadioButton.getText().toString();
-            userID = mAuth.getCurrentUser().getUid();
+                int selectedRadioButtonId = radioGroup.getCheckedRadioButtonId();
+                if (selectedRadioButtonId != -1) {
+                    selectedRadioButton = findViewById(selectedRadioButtonId);
+                    String selectedRbText = selectedRadioButton.getText().toString();
+                    userID = mAuth.getCurrentUser().getUid();
 //            textView.setText(selectedRbText + " is Selected");
 //            Toast.makeText(add_expense.this,userID + " User id Description -" + description.getText().toString() + " Amount-" + amount.getText().toString(),Toast.LENGTH_SHORT).show();
 
-            DocumentReference documentReference = fStore.collection("expenses").document(userID);
+                    DocumentReference documentReference = fStore.collection("expenses").document(userID);
 
-            Map<String,Object> user = new HashMap<>();
-            user.put("description",description.getText().toString() );
-            user.put("amount",amount.getText().toString());
-            user.put("splitType",selectedRbText);
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("description", description.getText().toString());
+                    user.put("amount", amount.getText().toString());
+                    user.put("splitType", selectedRbText);
 
-            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
-                @Override
-                public void onSuccess(Void unused) {
-                    Toast.makeText(add_expense.this,"Added Expense!",Toast.LENGTH_SHORT).show();
+                    documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(add_expense.this, "Added Expense!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }
-            });
-
-        }
-
+            }
+        });
     }
 }
