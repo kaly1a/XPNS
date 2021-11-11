9aebf1ceb2da363ddd9c7b34e1bf9ace8ad7e2ef
kaly1a <kalyaninayak1221@gmail.com>
2021-11-12 03:00:07 +0530

    ok

--- app/src/main/java/com/example/xpns/groups.java
+++ app/src/main/java/com/example/xpns/groups.java
@@ -8,7 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
 public class groups extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,12 @@ public class groups extends AppCompatActivity {
         setContentView(R.layout.activity_groups);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        mAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        String userID = mAuth.getCurrentUser().getUid();
+
+        Query query = firebaseFirestore.collection("expenses").whereEqualTo("userID",userID);
     }
     public void onClick(View view) {
         startActivity(new Intent(this, com.example.xpns.creatGroup.class));
