5daa8f69ea9e495255d01025d3a0bdc88f8b7a01
Akshay <cenahardy123@gmail.com>
2021-11-11 23:27:42 +0530

    Create Group 1/3 done

--- app/src/main/java/com/example/xpns/creatGroup.java
+++ app/src/main/java/com/example/xpns/creatGroup.java
@@ -5,15 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.Locale;
 
 public class creatGroup extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+    String userID,searchResultText,searchFieldText;
+    private FirebaseAuth mAuth;
+    FirebaseFirestore fStore ;
+    private ImageButton searchButton;
+    private EditText searchField;
+    private TextView searchResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +35,31 @@ public class creatGroup extends AppCompatActivity {
         setContentView(R.layout.activity_creat_group);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+        searchField = (EditText) findViewById(R.id.searchField);
+        searchResult = (TextView) findViewById(R.id.searchResult);
+        searchButton = (ImageButton) findViewById(R.id.search);
+        searchResult.setVisibility(View.INVISIBLE);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchFieldText = (String)searchField.getText().toString();
+                if (TextUtils.isEmpty(searchFieldText)) {
+                    searchField.setError("Please enter email to search");
+                }
+                else {
+
+                    fStore = FirebaseFirestore.getInstance();
+
+                    fStore.collection("users").document(searchFieldText).get().addOnSuccessListener(documentSnapshot -> {
+                        String user_name = documentSnapshot.getString("fName");
+                        searchResult.setText(user_name);
+                        searchResult.setVisibility(View.VISIBLE);
+                    });
+                }
+
+            }
+        });
         TextView textView2 =(TextView) findViewById(R.id.textView2);
         EditText grpname = (EditText) findViewById(R.id.groupName);
         ImageView grpIco = (ImageView) findViewById(R.id.groupIco);
@@ -57,6 +96,8 @@ public class creatGroup extends AppCompatActivity {
                }
 
             }
+
+
         });
     }
 
